bb56570c6f2da66840f188d1166549cdb2c74677
Komeil Majidi <k.majidi@esmcell.com>
2022-12-26 18:22:19 +0330

    ipfconfig
    library for default gateway ip discovery and configuration  for all operating systems for use in TSVPN

---
+++ ipfconfig/ipfconfig_darwin.go
@@ -0,0 +1,25 @@
+//go:build darwin
+// +build darwin
+package ipfconfig
+
+import (
+	"net"
+)
+
+func discoverGatewayOSSpecificIPv4() (ip net.IP, err error) {
+	ipstr := execCmd("sh", "-c", "route -n get default | grep 'gateway' | awk 'NR==1{print $2}'")
+	ipv4 := net.ParseIP(ipstr)
+	if ipv4 == nil {
+		return nil, errCantParse
+	}
+	return ipv4, nil
+}
+
+func discoverGatewayOSSpecificIPv6() (ip net.IP, err error) {
+	ipstr := execCmd("sh", "-c", "route -6 -n get default | grep 'gateway' | awk 'NR==1{print $2}'")
+	ipv6 := net.ParseIP(ipstr)
+	if ipv6 == nil {
+		return nil, errCantParse
+	}
+	return ipv6, nil
+}
---
+++ ipfconfig/ipfconfig_parsers.go
@@ -0,0 +1,123 @@
+package ipfconfig
+
+import (
+	"log"
+	"net"
+	"os/exec"
+	"strings"
+)
+
+type windowsRouteStructIPv4 struct {
+	Destination string
+	Netmask     string
+	Gateway     string
+	Interface   string
+	Metric      string
+}
+
+type windowsRouteStructIPv6 struct {
+	If          string
+	Metric      string
+	Destination string
+	Gateway     string
+}
+
+func parseToWindowsRouteStructIPv4(output []byte) (windowsRouteStructIPv4, error) {
+	lines := strings.Split(string(output), "\n")
+	sep := 0
+	for idx, line := range lines {
+		if sep == 3 {
+			// We just entered the 2nd section containing "Active Routes:"
+			if len(lines) <= idx+2 {
+				return windowsRouteStructIPv4{}, errNoGateway
+			}
+
+			fields := strings.Fields(lines[idx+2])
+			if len(fields) < 5 {
+				return windowsRouteStructIPv4{}, errCantParse
+			}
+
+			return windowsRouteStructIPv4{
+				Destination: fields[0],
+				Netmask:     fields[1],
+				Gateway:     fields[2],
+				Interface:   fields[3],
+				Metric:      fields[4],
+			}, nil
+		}
+		if strings.HasPrefix(line, "=======") {
+			sep++
+			continue
+		}
+	}
+	return windowsRouteStructIPv4{}, errNoGateway
+}
+
+func parseToWindowsRouteStructIPv6(output []byte) (windowsRouteStructIPv6, error) {
+
+	lines := strings.Split(string(output), "\n")
+	sep := 0
+	for idx, line := range lines {
+		if sep == 3 {
+			if len(lines) <= idx+2 {
+				return windowsRouteStructIPv6{}, errNoGateway
+			}
+
+			fields := strings.Fields(lines[idx+2])
+			if len(fields) < 4 {
+				return windowsRouteStructIPv6{}, errCantParse
+			}
+
+			return windowsRouteStructIPv6{
+				If:          fields[0],
+				Metric:      fields[1],
+				Destination: fields[2],
+				Gateway:     fields[3],
+			}, nil
+		}
+		if strings.HasPrefix(line, "=======") {
+			sep++
+			continue
+		}
+	}
+	return windowsRouteStructIPv6{}, errNoGateway
+}
+
+func parseWindowsGatewayIPv4(output []byte) (net.IP, error) {
+	parsedOutput, err := parseToWindowsRouteStructIPv4(output)
+	if err != nil {
+		return nil, err
+	}
+
+	ip := net.ParseIP(parsedOutput.Gateway)
+	if ip == nil {
+		return nil, errCantParse
+	}
+	return ip, nil
+}
+
+func parseWindowsGatewayIPv6(output []byte) (net.IP, error) {
+	parsedOutput, err := parseToWindowsRouteStructIPv6(output)
+	if err != nil {
+		return nil, err
+	}
+
+	ip := net.ParseIP(parsedOutput.Gateway)
+	if ip == nil {
+		return nil, errCantParse
+	}
+	return ip, nil
+}
+
+func execCmd(c string, args ...string) string {
+	cmd := exec.Command(c, args...)
+	out, err := cmd.Output()
+	if err != nil {
+		log.Println("failed to exec cmd:", err)
+	}
+	if len(out) == 0 {
+		return ""
+	}
+	s := string(out)
+	return strings.ReplaceAll(s, "\n", "")
+}
