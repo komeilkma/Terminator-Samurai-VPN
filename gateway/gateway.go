0c48af95106137390a1648bfb2d3aeeb74126698
Komeil Majidi <k.majidi@esmcell.com>
2023-01-16 23:40:46 +0330

    gateway discovering IP

---
+++ gateway/gateway.go
@@ -0,0 +1,21 @@
+package gateway
+
+import (
+	"errors"
+	"net"
+	"runtime"
+)
+
+var (
+	errNoGateway      = errors.New("no gateway found")
+	errCantParse      = errors.New("can't parse string output")
+	errNotImplemented = errors.New("not implemented for OS: " + runtime.GOOS)
+)
+
+func DiscoverGatewayIPv4() (ip net.IP, err error) {
+	return discoverGatewayOSSpecificIPv4()
+}
+
+func DiscoverGatewayIPv6() (ip net.IP, err error) {
+	return discoverGatewayOSSpecificIPv6()
+}
---
+++ gateway/gateway_linux.go
@@ -0,0 +1,23 @@
+package gateway
+
+import (
+	"net"
+)
+
+func discoverGatewayOSSpecificIPv4() (ip net.IP, err error) {
+	ipstr := execCmd("sh", "-c", "route -n | grep 'UG[ \t]' | awk 'NR==1{print $2}'")
+	ipv4 := net.ParseIP(ipstr)
+	if ipv4 == nil {
+		return nil, errCantParse
+	}
+	return ipv4, nil
+}
+
+func discoverGatewayOSSpecificIPv6() (ip net.IP, err error) {
+	ipstr := execCmd("sh", "-c", "route -6 -n | grep 'UG[ \t]' | awk 'NR==1{print $2}'")
+	ipv6 := net.ParseIP(ipstr)
+	if ipv6 == nil {
+		return nil, errCantParse
+	}
+	return ipv6, nil
+}
